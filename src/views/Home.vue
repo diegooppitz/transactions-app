c2618e711122aa122c0fa6b3eb523d5ac72ad97e
diegooppitz <diego.oppitz@gmail.com>
2022-02-23 14:07:28 -0300

    style home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,16 +14,16 @@
 
     <div class="tt__wrapper">
       <div class="tt__titles">
-          <p class="tt__item-title col-lg">Título</p>
-          <p class="tt__item-title col-lg">Descrição</p>
-          <p class="tt__item-title col-sm">Status</p>
-          <p class="tt__item-title col-sm">Valor</p>
+        <p class="tt__item-title col-lg">Título</p>
+        <p class="tt__item-title tt__item-desc col-lg">Descrição</p>
+        <p class="tt__item-title col-sm">Status</p>
+        <p class="tt__item-title col-sm">Valor</p>
       </div>
       <div v-for="(element, index) in data" :key="index" class="tt__line" @click="clickModal(element)">
-          <p class="tt__item col-lg">{{ element.title }}</p>
-          <p class="tt__item col-lg">{{ element.description }}</p>
-          <p class="tt__item col-sm">{{ formatStatus(element.status) }}</p>
-          <p class="tt__item col-sm">{{ formatAmount(element.amount) }}</p>
+        <p class="tt__item col-lg">{{ element.title }}</p>
+        <p class="tt__item tt__item-desc col-lg">{{ element.description }}</p>
+        <p class="tt__item col-sm">{{ formatStatus(element.status) }}</p>
+        <p class="tt__item col-sm">{{ formatAmount(element.amount) }}</p>
       </div>
     </div>
 
@@ -132,8 +132,14 @@ export default {
   border-radius: 20px;
   padding: 50px;
   margin: 15px;
+  width: 100%;
   min-height: 100vh;
   height: auto;
+
+  @media screen and (max-width: 768px) {
+    padding: 5px 0;
+    margin: 0;
+  }
 
   h1 {
     text-align: center;
@@ -141,25 +147,28 @@ export default {
 
   .tt__header {
     input {
-      height: 35px;
       width: 250px;
-      padding: 5px 10px;
-      outline: none;
-      box-sizing: border-box;
-      border-radius: 5px;
-      border: 1px #ccc solid;
     }
 
     select {
-      height: 35px;
-      width: 200px;
-      box-sizing: border-box;
-      outline: none;
-      border: solid 1px #ccc;
       background-color: #fff;
-      border-radius: 5px;
-      padding: 5px 10px;
       margin-left: 25px;
+      width: 200px;
+    }
+
+    input,
+    select {
+      height: 35px;
+      padding: 5px 10px;
+      border-radius: 5px;
+      outline: none;
+      box-sizing: border-box;
+      border: solid 1px #ccc;
+
+      @media screen and (max-width: 768px) {
+        width: 80%;
+        margin: 7px auto;
+      }
     }
   }
 
@@ -169,6 +178,10 @@ export default {
     justify-content: center;
     width: 70%;
     margin: 0 auto;
+
+    @media screen and (max-width: 768px) {
+      width: 95%;
+    }
 
     .tt__line,
     .tt__titles {
@@ -189,26 +202,45 @@ export default {
       }
     }
 
+    .tt__item-title {
+      font-size: 20px;
+
+      @media screen and (max-width: 768px) {
+        font-size: 16px;
+      }
+
+    }
+
     .tt__item {
       font-size: 18px;
       margin: 0;
+
+      @media screen and (max-width: 768px) {
+        font-size: 13px;
+      }
     }
 
-    .tt__item-title {
-        font-size: 20px;
+    .tt__item-desc {
+      @media screen and (max-width: 768px) {
+        display: none;
+      }
     }
   }
 }
 
 .col-lg {
-    width: 30%;
-}
+  width: 30%;
 
-.col-md {
-    width: 25%;
+  @media screen and (max-width: 768px) {
+    width: 32%;
+  }
 }
 
 .col-sm {
-    width: 20%;
+  width: 20%;
+
+  @media screen and (max-width: 768px) {
+    width: 32%;
+  }
 }
 </style>
